195c9bdac141ef04a07dbed89655d5cf382fc301
pdsong <songpeidong@gmail.com>
2023-09-05 05:11:32 +0800

    单例

---
+++ Creational/src/main/java/singleton/thread_safe/Singleton.java
@@ -0,0 +1,20 @@
+package singleton.thread_safe;
+
+public class Singleton {
+    private static volatile Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null)
+                    instance = new Singleton(value);
+            }
+        }
+        return instance;
+    }
+}
