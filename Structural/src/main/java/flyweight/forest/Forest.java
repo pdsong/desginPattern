a4ec44eadfc4e21048d91247b9ded4e49f8f96f2
pdsong <songpeidong@gmail.com>
2023-09-14 23:15:03 +0800

    享元模式

---
+++ Structural/src/main/java/flyweight/forest/Forest.java
@@ -0,0 +1,26 @@
+package flyweight.forest;
+
+import flyweight.trees.Tree;
+import flyweight.trees.TreeFactory;
+import flyweight.trees.TreeType;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        for(Tree tree:trees)
+            tree.draw(g);
+    }
+}
